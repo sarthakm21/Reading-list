deb0e90e1cb3ad80efd2e03d5f9406deae1619b0
sarthakm21 <sarthakmaheshwari140@gmail.com>
2020-06-19 08:59:32 +0530

    changes imports

--- src/components/reducers/BookReducer.js
+++ src/components/reducers/BookReducer.js
@@ -1,17 +1,20 @@
-import uuid from 'uuid/v1';
+const { v1: uuidv1 } = require('uuid');
 
 export const BookReducer = (state, action) => {
-    switch(action.type){
-        case 'addBook':
-            return [...state, {
-                title: action.book.title,
-                author: action.book.author,
-                id: uuid()
-            }]
-        case 'removeBook':
-            return state.filter(book => book.id!==action.id)
-        
-        default: 
-            return state;
-    }
-}
+	switch (action.type) {
+		case 'addBook':
+			return [
+				...state,
+				{
+					title: action.book.title,
+					author: action.book.author,
+					id: uuidv1()
+				}
+			];
+		case 'removeBook':
+			return state.filter((book) => book.id !== action.id);
+
+		default:
+			return state;
+	}
+};
